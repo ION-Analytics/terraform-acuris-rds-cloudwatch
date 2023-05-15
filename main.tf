5096b1656e7023f000197cabf326640edf892d04
Isaac Hollander <isaac.hollander@iongroup.com>
2023-05-15 14:26:44 -0400

    remove ClientId dimension

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ resource "aws_cloudwatch_metric_alarm" "free_storage_space" {
   alarm_description = "Alert when FreeStorageSpace <= 20480, 1 time within 1 minute"
   namespace         = "AWS/RDS"
   dimensions        = {
-    ClientId   = data.aws_caller_identity.current.account_id
     DbInstanceIdentifier = data.aws_db_instance.target.db_name
   }
   statistic           = "Minimum"
@@ -35,7 +34,6 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   alarm_description = "Alert when CPUUtilization >=${var.cpu_utilization_threshold}, ${var.cpu_utilization_evaluation_periods} time within ${var.cpu_utilization_period} seconds"
   namespace         = "AWS/RDS"
   dimensions        = {
-    ClientId   = data.aws_caller_identity.current.account_id
     DbInstanceIdentifier = data.aws_db_instance.target.db_name
   }
   statistic           = "Maximum"
