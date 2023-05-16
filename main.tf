b3b6516733ebcc7e39a15fb8576b850684c053c9
Isaac Hollander <isaac.hollander@iongroup.com>
2023-05-16 12:12:52 -0400

    Use correct database name

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ data "aws_db_instance" "target" {
 }
 
 resource "aws_sns_topic" "alarms" {
-  name = "${data.aws_db_instance.target.db_name}-alarms-topic"
+  name = "${data.aws_db_instance.target.db_instance_identifier}-alarms-topic"
 }
 
 resource "aws_cloudwatch_metric_alarm" "free_storage_space" {
-  alarm_name        = "${data.aws_db_instance.target.db_name} FreeStorageSpace <= 20480"
+  alarm_name        = "${data.aws_db_instance.target.db_instance_identifier} FreeStorageSpace <= 20480"
   alarm_description = "Alert when FreeStorageSpace <= 20480, 1 time within 1 minute"
   namespace         = "AWS/RDS"
   dimensions        = {
-    DBInstanceIdentifier = data.aws_db_instance.target.db_name
+    DBInstanceIdentifier = data.aws_db_instance.target.db_instance_identifier
   }
   statistic           = "Minimum"
   metric_name         = "FreeStorageSpace"
@@ -30,11 +30,11 @@ resource "aws_cloudwatch_metric_alarm" "free_storage_space" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
-  alarm_name        = "${data.aws_db_instance.target.db_name} CPUUtilization >= ${var.cpu_utilization_threshold}"
+  alarm_name        = "${data.aws_db_instance.target.db_instance_identifier} CPUUtilization >= ${var.cpu_utilization_threshold}"
   alarm_description = "Alert when CPUUtilization >=${var.cpu_utilization_threshold}, ${var.cpu_utilization_evaluation_periods} time within ${var.cpu_utilization_period} seconds"
   namespace         = "AWS/RDS"
   dimensions        = {
-    DBInstanceIdentifier = data.aws_db_instance.target.db_name
+    DBInstanceIdentifier = data.aws_db_instance.target.db_instance_identifier
   }
   statistic           = "Maximum"
   metric_name         = "CPUUtilization"
