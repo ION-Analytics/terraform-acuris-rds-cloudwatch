c6c374840ae98a351cbf97ad142e3cee6964c7ab
Isaac Hollander <isaac.hollander@iongroup.com>
2023-05-19 10:16:29 -0400

    Support differences between Aurora and classic RDS. Parametrize parameters for storage metric. Adjust defaults

--- main.tf
+++ main.tf
@@ -8,19 +8,23 @@ resource "aws_sns_topic" "alarms" {
   name = "${data.aws_db_instance.target.db_instance_identifier}-alarms-topic"
 }
 
+locals {
+  cluster_dimension = tomap({DBClusterIdentifier = data.aws_db_instance.target.db_cluster_identifier})
+  noncluster_dimension = tomap({DBInstanceIdentifier = data.aws_db_instance.target.db_instance_identifier})
+}
+
 resource "aws_cloudwatch_metric_alarm" "free_storage_space" {
   alarm_name        = "${data.aws_db_instance.target.db_instance_identifier} FreeStorageSpace <= 20480"
   alarm_description = "Alert when FreeStorageSpace <= 20480, 1 time within 1 minute"
   namespace         = "AWS/RDS"
-  dimensions        = {
-    DBInstanceIdentifier = data.aws_db_instance.target.db_instance_identifier
-  }
+  dimensions        = startswith(data.aws_db_instance.target.engine,"aurora") ? local.cluster_dimension : local.noncluster_dimension
   statistic           = "Minimum"
-  metric_name         = "FreeStorageSpace"
+  metric_name         = startswith(data.aws_db_instance.target.engine,"aurora") ? "FreeLocalStorage" : "FreeStorageSpace"
   comparison_operator = "LessThanOrEqualToThreshold"
   threshold           = var.free_storage_space_threshold 
-  period              = 60
-  evaluation_periods  = 3
+  period              = var.free_storage_space_period
+  evaluation_periods  = var.free_storage_space_evaluation_periods
+  treat_missing_data  = "ignore"
   ok_actions          = [
     aws_sns_topic.alarms.arn
   ]
@@ -33,15 +37,14 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   alarm_name        = "${data.aws_db_instance.target.db_instance_identifier} CPUUtilization >= ${var.cpu_utilization_threshold}"
   alarm_description = "Alert when CPUUtilization >=${var.cpu_utilization_threshold}, ${var.cpu_utilization_evaluation_periods} time within ${var.cpu_utilization_period} seconds"
   namespace         = "AWS/RDS"
-  dimensions        = {
-    DBInstanceIdentifier = data.aws_db_instance.target.db_instance_identifier
-  }
+  dimensions        = startswith(data.aws_db_instance.target.engine,"aurora") ? local.cluster_dimension : local.noncluster_dimension
   statistic           = "Maximum"
   metric_name         = "CPUUtilization"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold           = var.cpu_utilization_threshold
   period              = var.cpu_utilization_period
   evaluation_periods  = var.cpu_utilization_evaluation_periods
+  treat_missing_data  = "ignore"
   ok_actions          = [
     aws_sns_topic.alarms.arn
   ]
--- variables.tf
+++ variables.tf
@@ -8,6 +8,18 @@ variable "free_storage_space_threshold" {
   default = 20480  # 20 megabytes.  might want to adjust based on actual storage amount
 }
 
+# duration in minutes of sampling period for cpu utilization check
+variable "free_storage_space_period" {
+  type = string
+  default = 60
+}
+
+# number of cpu_utilization_periods exceeding cpu_utilization_threshold before triggering alarm
+variable "free_storage_space_evaluation_periods" {
+  type = string
+  default = 5
+}
+
 # cpu utilization percentage threshold
 variable "cpu_utilization_threshold" {
   type = string
@@ -17,11 +29,13 @@ variable "cpu_utilization_threshold" {
 # duration in minutes of sampling period for cpu utilization check
 variable "cpu_utilization_period" {
   type = string
-  default = 30
+  default = 60
 }
 
 # number of cpu_utilization_periods exceeding cpu_utilization_threshold before triggering alarm
 variable "cpu_utilization_evaluation_periods" {
   type = string
-  default = 3
+  default = 5
 }
+
+
